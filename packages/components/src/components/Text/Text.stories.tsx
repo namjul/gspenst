07663fdd1208aee2d5838039ff1d8128fe8b1195
Samuel Hobl <nam@hobl.at>
2021-09-08 15:37:11 +0200

    feat(components): add `Text` component

--- packages/components/src/components/Text/Text.stories.tsx
+++ packages/components/src/components/Text/Text.stories.tsx
@@ -9,4 +9,38 @@ const meta: Meta = {
 
 export default meta
 
-export const Default: React.FC = () => <Text>Text</Text>
+export const Default = () => <Text>Text</Text>
+
+export const OtherElements = () => {
+  return (
+    <>
+      <Text as="strong">Bold</Text>
+      <br />
+      <Text as="i">Italic</Text>
+      <br />
+      <Text as="u">Underline</Text>
+      <br />
+      <Text as="abbr">I18N</Text>
+      <br />
+      <Text as="cite">Citation</Text>
+      <br />
+      <Text as="del">Deleted</Text>
+      <br />
+      <Text as="em">Emphasis</Text>
+      <br />
+      <Text as="ins">Inserted</Text>
+      <br />
+      <Text as="kbd">Ctrl + C</Text>
+      <br />
+      <Text as="mark">Highlighted</Text>
+      <br />
+      <Text as="s">Strikethrough</Text>
+      <br />
+      <Text as="samp">Sample</Text>
+      <br />
+      <Text as="sub">sub</Text>
+      <br />
+      <Text as="sup">sup</Text>
+    </>
+  )
+}
---
+++ packages/components/src/components/Text/Text.styles.ts
@@ -0,0 +1,5 @@
+import type { CSS } from '../../stitches.config'
+
+export const styles: CSS = {
+  color: '$text',
+}
