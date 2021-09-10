193cecd3d7b4ff58eb7d715a2ed293d844f7cd13
Samuel Hobl <nam@hobl.at>
2021-09-10 17:47:41 +0200

    chore: typo

--- starters/next-starter-typescript/src/pages/index.tsx
+++ starters/next-starter-typescript/src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 import { View } from '@gspenst/components'
 
 const IndexPage: NextPage = (props) => {
-  return <View>@gspenst/next-starter</View>
+  return <View>@gspenst/next-starter-typescript</View>
 }
 
 export default IndexPage
