2926d5f862ffc74a504e146af4a51a71f8d51c48
Samuel Hobl <nam@hobl.at>
2021-09-10 11:56:05 +0200

    chore: initialize starters

---
+++ starters/next-starter-gspenst/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  // experimental: {
+  //   externalDir: true,
+  // },
+  // webpack: (config) => {
+  //   config.module.rules.push({
+  //     test: /\.ts?$/,
+  //     use: ['ts-loader'],
+  //   })
+  //
+  //   return config
+  // },
+  reactStrictMode: true,
+}
