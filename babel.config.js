5427e9e388ea24d66190972b050c12e699a899b5
Samuel Hobl <nam@hobl.at>
2021-09-13 13:45:17 +0200

    spike(audit): setup launch chrome

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/react', '@babel/preset-typescript'],
+  babelrcRoots: [
+    // Keep the root as a root
+    '.',
+
+    // Also consider monorepo packages "root" and load their .babelrc.json files.
+    './packages/*',
+  ],
 }
--- packages/audit/src/cli.ts
+++ packages/audit/src/cli.ts
@@ -1,3 +1,12 @@
-export const [, , ...args] = process.argv
+// import lighthouse from 'lighthouse'
+import * as chromeLauncher from 'chrome-launcher'
 
-console.log(`Hello World ${args}`)
+const launchChrome = async (url: string) => {
+  const chrome = await chromeLauncher.launch({
+    startingUrl: url,
+  })
+
+  setTimeout(() => chrome.kill(), 3000)
+}
+
+void launchChrome('http://localhost:5000')
