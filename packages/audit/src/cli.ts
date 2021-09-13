bb518d2e6843b99ca67fa0f749bcd8c5e3a4af9f
Samuel Hobl <nam@hobl.at>
2021-09-13 14:44:12 +0200

    spike(audit): run lighthouse

--- packages/audit/src/cli.ts
+++ packages/audit/src/cli.ts
@@ -1,12 +1,22 @@
-// import lighthouse from 'lighthouse'
+/* eslint-disable @typescript-eslint/no-explicit-any  */
+
+import lighthouse from 'lighthouse'
 import * as chromeLauncher from 'chrome-launcher'
 
 const launchChrome = async (url: string) => {
-  const chrome = await chromeLauncher.launch({
-    startingUrl: url,
-  })
+  const chrome = await chromeLauncher.launch()
 
-  setTimeout(() => chrome.kill(), 3000)
+  const opts = {
+    port: chrome.port,
+  }
+  const { report } = await lighthouse(url, opts)
+
+  await chrome.kill()
+  console.log(report)
+
+  return report
 }
 
-void launchChrome('http://localhost:5000')
+void launchChrome('http://localhost:5000').then((result) => {
+  console.log(result)
+})
