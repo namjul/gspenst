f9a3f79a8441a46e0ab3f7cd32a780ea0dc96755
Samuel Hobl <nam@hobl.at>
2021-09-24 10:01:19 +0200

    chore: remove gatsby starter
    I won't focus now on a gatsby solution.

--- starters/gatsby-starter-typescript/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import type { PageProps } from 'gatsby'
-import { View } from '@gspenst/components'
-
-const IndexPage = (props: PageProps) => {
-  return <View>@gspenst/gatsby-starter-typescript</View>
-}
-
-export default IndexPage
