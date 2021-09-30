11662281180bf393a70c064845ae8aa585e5ef59
Samuel Hobl <nam@hobl.at>
2021-09-30 12:35:03 +0200

    chore: fix eslint issue
    `paths` in packages where not resolved.

--- packages/next/types/nextra.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'nextra' {
-  import type { NextConfig } from 'next'
-  function withNextra(
-    ...args: string[] | { theme: string; themeConfig: string }[]
-  ): (nextConfig: NextConfig) => NextConfig
-  export default withNextra
-}
