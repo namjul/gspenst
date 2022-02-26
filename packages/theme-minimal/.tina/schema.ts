9b74002ab53a4ed85520dde3ab96de3a93587658
Samuel Hobl <nam@hobl.at>
2022-02-26 21:27:09 +0100

    fix(theme-minimal): make adding content sections with default value work
    There seems to be a bug when using `defaultItem` with `rich-text` fields.

--- packages/theme-minimal/.tina/schema.ts
+++ packages/theme-minimal/.tina/schema.ts
@@ -4,11 +4,6 @@ import type { TinaTemplate } from '@tinacms/cli'
 const contentSectionSchema: TinaTemplate = {
   name: 'content',
   label: 'Content',
-  ui: {
-    defaultItem: {
-      body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.',
-    },
-  },
   fields: [
     {
       type: 'rich-text',
