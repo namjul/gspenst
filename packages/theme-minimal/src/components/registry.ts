30db1ccce69311fc3ca088caae29fdf7b08ecd8d
Samuel Hobl <nam@hobl.at>
2022-03-02 14:02:29 +0100

    chore(theme-minimal): Remove unnecessary code
    Not sure why this works now.

--- packages/theme-minimal/src/components/registry.ts
+++ packages/theme-minimal/src/components/registry.ts
@@ -1,22 +1,15 @@
 import dynamic from 'next/dynamic'
 import { ComponentType } from 'react'
 import type { LiteralUnion } from '@gspenst/utils'
-// import PageLayout from './components/PageLayout';
 
+// Inspiration:
 // https://docs.stackbit.com/conceptual-guides/components/registering/
 // https://github.com/stackbit-themes/starter-nextjs-theme/blob/main/src/components/components-registry.ts
 // https://nextjs.org/docs/advanced-features/dynamic-import
-// preconstruct does not add `__esmodule` flag therefore mapping to `mod.default`
 
 export const components = {
-  PageLayout: dynamic(() => {
-    const Comp = import('./layouts/PageLayout').then((mod) => mod.default)
-    return Comp
-  }),
-  PostLayout: dynamic(() => {
-    const Comp = import('./layouts/PostLayout').then((mod) => mod.default)
-    return Comp
-  }),
+  PageLayout: dynamic(() => import('./layouts/PageLayout')),
+  PostLayout: dynamic(() => import('./layouts/PostLayout')),
 }
 
 export default function getComponent<
