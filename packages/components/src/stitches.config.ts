f502b5dea6c5f85cd183b4463e104d9a4771c626
Samuel Hobl <nam@hobl.at>
2022-03-05 15:49:12 +0100

    feat(theme-minimal): add css reset styles

--- packages/components/src/stitches.config.ts
+++ packages/components/src/stitches.config.ts
@@ -39,4 +39,66 @@ export type Theme = ReturnType<typeof createTheme>
 export const lightTheme = createTheme(LIGHT_THEME, lightThemeConfig)
 export const darkTheme = createTheme(DARK_THEME, darkThemeConfig)
 
-export const globalStyles = globalCss({})
+export const globalStyles = globalCss({
+  // From https://www.joshwcomeau.com/css/custom-css-reset/
+
+  /*
+  Use a more -intuitive box-sizing mode.
+  */
+  '*, *::before, *::after': {
+    boxSizing: 'border-box',
+  },
+
+  /*
+  Remove default marings
+  */
+  '*': {
+    margin: 0,
+  },
+
+  /*
+  Allow percentage-based heights in the application
+  */
+  'html, body, #__next': {
+    height: '100%',
+  },
+
+  /*
+  Typographic tweaks!
+  4. Add accessible line-height
+  5. Improve text rendering
+  */
+  body: {
+    lineHeight: 1.5,
+    '-webkit-font-smoothing': 'antialiased',
+  },
+
+  /*
+  6. Improve media defaults
+  */
+  'img, picture, video, canvas, svg': {
+    display: 'block',
+    maxWidth: '100%',
+  },
+
+  /*
+  7. Remove built-in form typography styles
+  */
+  'input, button, textarea, select': {
+    font: 'inherit',
+  },
+
+  /*
+  8. Avoid text overflows
+  */
+  'p, h1, h2, h3, h4, h5, h6': {
+    overflowWrap: 'break-word',
+  },
+
+  /*
+  9. Create a root stacking context
+  */
+  '#root, #__next': {
+    isolation: 'isolate',
+  },
+})
