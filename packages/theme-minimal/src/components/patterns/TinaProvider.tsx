ca41e6f44a9335a6a3ad70782eda3367f86dc9fc
Samuel Hobl <nam@hobl.at>
2022-03-06 20:19:41 +0100

    fix(theme-minimal): Remove unnecessary dynamic import

--- packages/theme-minimal/src/components/patterns/TinaProvider.tsx
+++ packages/theme-minimal/src/components/patterns/TinaProvider.tsx
@@ -1,7 +1,5 @@
-import dynamic from 'next/dynamic'
+import TinaCMS from 'tinacms'
 import { tinaConfig } from '../../../.tina/schema'
-
-const TinaCMS = dynamic(() => import('tinacms'), { ssr: false })
 
 const TinaProvider = ({ children }: React.PropsWithChildren<{}>) => {
   // @ts-expect-error -- actually I dont' expect an error but could not find the reason for it.
