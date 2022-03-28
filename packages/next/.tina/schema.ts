a3010002282c3fdc5b6bd533fa265df7b84848a9
Samuel Hobl <nam@hobl.at>
2022-03-28 15:50:47 +0200

    chore(next): cleanup

--- packages/next/.tina/schema.ts
+++ packages/next/.tina/schema.ts
@@ -1,5 +1,5 @@
 import { defineSchema, defineConfig } from 'tinacms'
-// import type { TinaTemplate } from 'tinacms'
+import type { TinaTemplate, TinaCollection } from 'tinacms'
 
 // const testimonialBlockSchema: TinaTemplate = {
 //   name: 'BlockQuote',
@@ -33,108 +33,116 @@ import { defineSchema, defineConfig } from 'tinacms'
 //   ],
 // }
 
+const configCollection: TinaCollection = {
+  label: 'Config',
+  name: 'config',
+  path: 'content/config',
+  format: 'json',
+  fields: [
+    {
+      type: 'boolean',
+      label: 'Dark Mode',
+      name: 'darkMode',
+    },
+  ],
+}
+
+const pageCollection: TinaCollection = {
+  label: 'Pages',
+  name: 'page',
+  path: 'content/pages',
+  format: 'mdx',
+  fields: [
+    {
+      type: 'string',
+      label: 'Title',
+      name: 'title',
+    },
+    {
+      type: 'rich-text',
+      label: 'Body',
+      name: 'body',
+      templates: [],
+      isBody: true,
+    },
+  ],
+}
+
+const postCollection: TinaCollection = {
+  label: 'Blog Posts',
+  name: 'post',
+  path: 'content/posts',
+  format: 'mdx',
+  fields: [
+    {
+      type: 'string',
+      label: 'Title',
+      name: 'title',
+    },
+    {
+      type: 'image',
+      name: 'heroImg',
+      label: 'Hero Image',
+    },
+    {
+      type: 'rich-text',
+      label: 'Excerpt',
+      name: 'excerpt',
+    },
+    {
+      type: 'reference',
+      label: 'Author',
+      name: 'author',
+      collections: ['author'],
+    },
+    {
+      type: 'datetime',
+      label: 'Posted Date',
+      name: 'date',
+      ui: {
+        dateFormat: 'MMMM DD YYYY',
+        timeFormat: 'hh:mm A',
+      },
+    },
+    {
+      type: 'rich-text',
+      label: 'Body',
+      name: 'body',
+      templates: [],
+      isBody: true,
+    },
+  ],
+}
+
+const authorCollection: TinaCollection = {
+  label: 'Authors',
+  name: 'author',
+  path: 'content/authors',
+  fields: [
+    {
+      type: 'string',
+      label: 'Name',
+      name: 'name',
+    },
+    {
+      type: 'string',
+      label: 'Title',
+      name: 'title',
+    },
+    {
+      type: 'string',
+      label: 'Avatar',
+      name: 'avatar',
+    },
+  ],
+}
+
 export default defineSchema({
   collections: [
-    {
-      label: 'Config',
-      name: 'config',
-      path: 'content/config',
-      format: 'json',
-      fields: [
-        {
-          type: 'boolean',
-          label: 'Dark Mode',
-          name: 'darkMode',
-        },
-      ],
-    },
-    {
-      label: 'Blog Posts',
-      name: 'post',
-      path: 'content/posts',
-      format: 'mdx',
-      fields: [
-        {
-          type: 'string',
-          label: 'Title',
-          name: 'title',
-        },
-        {
-          type: 'image',
-          name: 'heroImg',
-          label: 'Hero Image',
-        },
-        {
-          type: 'rich-text',
-          label: 'Excerpt',
-          name: 'excerpt',
-        },
-        {
-          type: 'reference',
-          label: 'Author',
-          name: 'author',
-          collections: ['author'],
-        },
-        {
-          type: 'datetime',
-          label: 'Posted Date',
-          name: 'date',
-          ui: {
-            dateFormat: 'MMMM DD YYYY',
-            timeFormat: 'hh:mm A',
-          },
-        },
-        {
-          type: 'rich-text',
-          label: 'Body',
-          name: 'body',
-          templates: [],
-          isBody: true,
-        },
-      ],
-    },
-    {
-      label: 'Authors',
-      name: 'author',
-      path: 'content/authors',
-      fields: [
-        {
-          type: 'string',
-          label: 'Name',
-          name: 'name',
-        },
-        {
-          type: 'string',
-          label: 'Title',
-          name: 'title',
-        },
-        {
-          type: 'string',
-          label: 'Avatar',
-          name: 'avatar',
-        },
-      ],
-    },
-    {
-      label: 'Pages',
-      name: 'page',
-      path: 'content/pages',
-      format: 'mdx',
-      fields: [
-        {
-          type: 'string',
-          label: 'Title',
-          name: 'title',
-        },
-        {
-          type: 'rich-text',
-          label: 'Body',
-          name: 'body',
-          templates: [],
-          isBody: true,
-        },
-      ],
-    },
+    configCollection,
+    pageCollection,
+    postCollection,
+    authorCollection,
   ],
 })
 
