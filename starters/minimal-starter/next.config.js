e99d6c8974d96aee3f8210c53a4f7c24a13e6827
Samuel Hobl <nam@hobl.at>
2022-04-29 16:49:28 +0200

    chore(minimal-starter): add lint command

--- starters/minimal-starter/next.config.js
+++
@@ -1,3 +0,0 @@
-const { default: withGspenst } = require('@gspenst/next')
-
-module.exports = withGspenst('@gspenst/nextra-theme-blog')({})
--- starters/minimal-starter/next.config.mjs
+++ starters/minimal-starter/next.config.mjs
@@ -1,12 +1,3 @@
 import withGspenst from '@gspenst/next'
 
-export default withGspenst.default('./theme')({
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-
-})
+export default withGspenst.default('./theme')({})
