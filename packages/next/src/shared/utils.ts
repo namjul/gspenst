d5605ca3f86d826523a66ab0211eb004e0735300
Samuel Hobl <nam@hobl.at>
2022-06-04 14:02:05 +0200

    chore: commit missing part

--- packages/next/src/shared/utils.ts
+++ packages/next/src/shared/utils.ts
@@ -19,6 +19,10 @@ export function isString(arg: unknown): arg is string {
   return typeof arg === 'string'
 }
 
+export function isNumber(arg: unknown): arg is number {
+  return typeof arg === 'number'
+}
+
 export function ensureString(value: unknown): asserts value is string {
   if (typeof value !== 'string') {
     throw new Error("Value isn't a string type")
