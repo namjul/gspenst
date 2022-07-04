8489f8acd38ed6c27c18e473f6e9ee95b056d2b5
Samuel Hobl <nam@hobl.at>
2022-07-04 14:50:05 +0200

    chore(gspenst): remove outdated comment

--- packages/gspenst/src/shared/client.ts
+++ packages/gspenst/src/shared/client.ts
@@ -8,8 +8,6 @@ const apiURL =
     ? `https://content.tinajs.io/content/${tinaClientId}/github/${branch}`
     : 'http://localhost:4001/graphql'
 
-// TODO use queries from generated types
-// See: https://tina.io/docs/graphql/read-only-tokens/#making-requests-with-the-tina-client
 export const client = createClient({
   queries,
   url: apiURL,
