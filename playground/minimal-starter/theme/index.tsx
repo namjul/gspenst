e53f9091706bad203fdd64b53dd7e251a49d7a10
Samuel Hobl <nam@hobl.at>
2022-07-22 13:58:15 +0200

    chore(playground): use react-json-view

--- playground/minimal-starter/theme/index.tsx
+++ playground/minimal-starter/theme/index.tsx
@@ -1,14 +1,14 @@
-// import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic'
 import { useStore } from 'gspenst/data'
 
-// const DynamicReactJson = dynamic(import('react-json-view'), { ssr: false })
+const DynamicReactJson = dynamic(import('react-json-view'), { ssr: false })
 
 const createLayout = () => {
   const Page = () => {
     const { state } = useStore()
     return (
       <div>
-        <pre>{JSON.stringify(state, null, 2)}</pre>
+        <DynamicReactJson src={state} collapsed={true} />
       </div>
     )
   }
