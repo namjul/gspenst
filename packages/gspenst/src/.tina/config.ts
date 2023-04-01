383c169933ac36fc27cafb1f5ff5ae05924ec1e3
Samuel Hobl <samuel.hobl@gmail.com>
2023-04-01 19:17:32 +0200

    chore(gspenst): revert new tina config rearrangement and move into `src`
    tina has a bug with new config location + `rootPath` flag

---
+++ packages/gspenst/src/.tina/config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "../tina-config";
+import { defineSchema } from '../tina-schema'
+
+export default defineConfig({
+  schema: defineSchema()
+})
--- packages/gspenst/tina/config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "../src/tina-config";
-import { defineSchema } from '../src/tina-schema'
-
-export default defineConfig({
-  schema: defineSchema()
-})
