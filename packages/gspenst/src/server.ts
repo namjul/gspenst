67f0cdead60936e9fc5e4592b5b60e51d2455f4b
Samuel Hobl <samuel.hobl@gmail.com>
2023-05-06 12:25:29 +0200

    chore(gspenst): relocate function

--- packages/gspenst/src/server.ts
+++ packages/gspenst/src/server.ts
@@ -1,3 +1,4 @@
+import EventEmitter, { once } from 'events'
 import {
   parseRoutesWithDefaults as parseRoutes,
   type RoutesConfigInput,
@@ -52,6 +53,42 @@ export const getProps = async (config: Config, params: string | string[]) => {
   )
 }
 
+
+export async function startTinaServer(
+  this: { projectPath: string } & EventEmitter,
+  config: { onlyCheck: boolean }
+) {
+  if (!config.onlyCheck) {
+    log('Starting tina server', this.projectPath)
+
+    const ps = await startSubprocess({
+      command: 'tinacms dev',
+      cwd: this.projectPath,
+    })
+
+    this.on('cleanup', () => {
+      ps?.kill()
+    })
+
+    // TODO output errors
+    if (ps?.stdout) {
+      let flag = true
+      while (flag) {
+        const [data] = (await once(ps.stdout, 'data')) as Buffer[] // eslint-disable-line no-await-in-loop
+        const msg = data?.toString().trim()
+        if (msg) {
+          log(msg)
+          // eslint-disable-next-line max-depth
+          if (msg.includes('4001')) {
+            flag = false
+          }
+        }
+      }
+    }
+
+    return ps
+  }
+}
+
 export { collect } from './collect'
-export { startTinaServer } from './tina-server'
 export { routerManager, controller, repository, parseRoutes }
