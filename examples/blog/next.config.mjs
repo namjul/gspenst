7bff781983bcf1588418a6be078514d5ef0e23dd
Samuel Hobl <samuel.hobl@gmail.com>
2023-05-11 11:12:13 +0200

    chore: little things

--- examples/blog/next.config.mjs
+++ examples/blog/next.config.mjs
@@ -3,8 +3,4 @@ import withNextBundleAnalyzer from 'next-bundle-analyzer'
 
 export default withNextBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
-})(
-  withGspenst.default('@gspenst/nextra-theme-blog')({
-    output: 'export',
-  })
-)
+})(withGspenst.default('@gspenst/nextra-theme-blog')({}))
