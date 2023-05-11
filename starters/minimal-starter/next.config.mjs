fb5303cfa27619e9be5c7eb9a5d6706f0704a5c8
Samuel Hobl <samuel.hobl@gmail.com>
2023-05-11 18:25:30 +0200

    chore: update starter

--- starters/minimal-starter/next.config.mjs
+++ starters/minimal-starter/next.config.mjs
@@ -1,11 +1,3 @@
 import withGspenst from '@gspenst/next'
-// import withNextBundleAnalyzer from '@next/bundle-analyzer'
-import withNextBundleAnalyzer from 'next-bundle-analyzer'
 
-export default withNextBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})(
-  withGspenst.default('./theme')({
-    productionBrowserSourceMaps: true,
-  })
-)
+export default withGspenst.default('./theme')({})
