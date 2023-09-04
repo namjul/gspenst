1b71a931363d2f5e047d96091bf74bf6049ef094
Samuel Hobl <samuel.hobl@gmail.com>
2023-09-04 18:39:04 +0200

    chore(gspenst): ensure `routesConfig`

--- packages/gspenst/src/server.ts
+++ packages/gspenst/src/server.ts
@@ -1,4 +1,5 @@
 import EventEmitter, { once } from 'events'
+import { okAsync, errAsync } from './shared/kernel'
 import path from 'path'
 import fse from 'fs-extra'
 import {
@@ -14,20 +15,26 @@ import { createLoaders } from './dataLoader'
 import { getPageMap, type PageMapItem } from './helpers/getPageMap'
 import { createLogger } from './logger'
 import { startSubprocess } from './utils'
+import * as Errors from './errors'
 
 const log = createLogger(`server`)
 
 export const build = (routesConfigInput: RoutesConfigInput) => {
-  return repository.collect(routesConfigInput).map((resources) => {
+  return repository.collect(routesConfigInput).andThen((resources) => {
     const routesResource = resources.find(isRoutesResource)
-    const pageMap = getPageMap(resources)
-    updateRoutingMapping(pageMap)
 
-    repository.notify((resource: any) => {
-      console.log('new resource', resource)
-    })
+    if (routesResource) {
+      const pageMap = getPageMap(resources)
+      updateRoutingMapping(pageMap)
 
-    return { pageMap, routesConfig: routesResource?.data }
+      repository.notify((resource: any) => {
+        console.log('new resource', resource)
+      })
+
+      return okAsync({ pageMap, routesConfig: routesResource.data })
+    } else {
+      return errAsync(Errors.notFound('routesResource not found'))
+    }
   })
 }
 
@@ -45,7 +52,7 @@ export const getPaths = (config: Config) => {
     const locatorResources = resources.filter(filterLocatorResources)
     // TODO validate config.routesConfig, because there can be references that actually don't exist
     const paths = routerManager(config.routesConfig).resolvePaths(
-      locatorResources
+      locatorResources,
     )
     log('getPaths: ', JSON.stringify(paths, null, 2))
     return paths
@@ -59,13 +66,13 @@ export const getProps = async (config: Config, params: string | string[]) => {
 
   return controller(
     routingContext,
-    createLoaders(undefined, config.isBuildPhase)
+    createLoaders(undefined, config.isBuildPhase),
   )
 }
 
 export async function startTinaServer(
   this: { projectPath: string } & EventEmitter,
-  config: { onlyCheck: boolean }
+  config: { onlyCheck: boolean },
 ) {
   if (!config.onlyCheck) {
     log('Starting tina server', this.projectPath)
@@ -102,7 +109,7 @@ export async function startTinaServer(
 export function updateRoutingMapping(pageMap: PageMapItem[]) {
   log('Writing `routingMapping.json`...')
   const packagePath: string = path.dirname(
-    require.resolve(`gspenst/package.json`)
+    require.resolve(`gspenst/package.json`),
   )
   const routingMappingFilePath = path.resolve(packagePath, './pageMap.json')
   return fse.writeJsonSync(routingMappingFilePath, pageMap)
